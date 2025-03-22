e76de04a107874d58d22b7f7d29d223a749b2ce8
Omar Aizab <omaraizab98@gmail.com>
2025-03-22 00:08:26 +0000

    initeal commit

---
+++ utils/setup.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Define text color variables
+BLACK="\033[30m"
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+BLUE="\033[34m"
+MAGENTA="\033[35m"
+CYAN="\033[36m"
+WHITE="\033[37m"
+NC="\033[0m" # No Color
+
+# Define background color variables
+BG_BLACK="\033[40m"
+BG_RED="\033[41m"
+BG_GREEN="\033[42m"
+BG_YELLOW="\033[43m"
+BG_BLUE="\033[44m"
+BG_MAGENTA="\033[45m"
+BG_CYAN="\033[46m"
+BG_WHITE="\033[47m"
+
+echo -e "${BLUE}Setting up the system...${NC}"
+
+# Add repositories
+echo -e "${YELLOW}Adding VirtualBox repository...${NC}"
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
+wget -O- https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo gpg --yes --output /usr/share/keyrings/oracle-virtualbox-2016.gpg --dearmor
+
+echo -e "${YELLOW}Adding HashiCorp repository for Vagrant...${NC}"
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+# Update package list
+echo -e "${CYAN}Updating package list...${NC}"
+sudo apt update -y
+
+# Install VirtualBox
+echo -e "${GREEN}Installing VirtualBox...${NC}"
+sudo apt install -y "virtualbox-7.1"
+
+# Install Vagrant
+echo -e "${GREEN}Installing Vagrant...${NC}"
+sudo apt install -y vagrant
